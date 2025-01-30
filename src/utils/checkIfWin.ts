8b477b3aae897ce8c1573b4fa8afd6e24e165e4a
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2025-01-30 23:15:04 +0200

    removed console

--- src/utils/checkIfWin.ts
+++ src/utils/checkIfWin.ts
@@ -1,13 +1,11 @@
 import { BoardCell } from '../types';
 
 export function checkIfWin(board: BoardCell[][]) {
-  const result = board.every((row) =>
+  return board.every((row) =>
     row.every(
       (cell) =>
         (cell.value === '💣' && !cell.revealed) ||
         (cell.value !== '💣' && cell.revealed)
     )
   );
-  console.log(result);
-  return result;
 }
